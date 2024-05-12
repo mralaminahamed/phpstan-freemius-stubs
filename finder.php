967f5d580fa1f8f29d03de6c4cc81da0e60726ad
Viktor Szépe <viktor@szepe.net>
2024-05-12 15:13:36 +0000

    Improve stubs

--- finder.php
+++ finder.php
@@ -1,33 +1,11 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
 return \StubsGenerator\Finder::create()
     ->in( array(
         'source/vendor/freemius/wordpress-sdk/includes',
-        'source/vendor/freemius/wordpress-sdk/templates'
     ) )
-    ->append(
-        \StubsGenerator\Finder::create()
-            ->in(['source/vendor/freemius/wordpress-sdk'])
-            ->files()
-            ->depth('< 1')
-            ->path('config.php')
-    )
-    ->append(
-        \StubsGenerator\Finder::create()
-            ->in(['source/vendor/freemius/wordpress-sdk/includes/'])
-            ->files()
-            ->depth('< 1')
-            ->path('fs-essential-functions.php')
-    )
-    ->append(
-        \StubsGenerator\Finder::create()
-            ->in(['source/vendor/freemius/wordpress-sdk/includes/'])
-            ->files()
-            ->depth('< 1')
-            ->path('require.php')
-    )
+    ->notPath('customizer')
+    ->notPath('debug')
     ->append(
         \StubsGenerator\Finder::create()
             ->in(['source/vendor/freemius/wordpress-sdk'])
@@ -35,7 +13,5 @@ return \StubsGenerator\Finder::create()
             ->depth('< 1')
             ->path('start.php')
     )
-    ->notPath('source/vendor/freemius/wordpress-sdk/includes/customizer')
-    ->notPath('source/vendor/freemius/wordpress-sdk/includes/debug')
     ->sortByName(true)
 ;
