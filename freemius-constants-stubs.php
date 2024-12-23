a464efa1c245156a2abcc6c1553ae7efcd6ec2c6
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-12-23 21:46:16 +0600

    Generate stubs for Freemius WordPress SDK 1.1.6.5

--- freemius-constants-stubs.php
+++ freemius-constants-stubs.php
@@ -18,19 +18,7 @@
 \define('FS_SDK__SIMULATE_NO_API_CONNECTIVITY_CLOUDFLARE', \true);
 \define('FS_SDK__SIMULATE_NO_API_CONNECTIVITY_SQUID_ACL', \true);
 // VVV default public network IP.
-\define('WP_FS__VVV_DEFAULT_PUBLIC_IP', '192.168.50.1');
-/**
- * If true and running with secret key, the opt-in process
- * will skip the email activation process which is invoked
- * when the email of the context user already exist in Freemius
- * database (as a security precaution, to prevent sharing user
- * secret with unauthorized entity).
- *
- * IMPORTANT:
- *      AS A SECURITY PRECAUTION, WE VALIDATE THE TIMESTAMP OF THE OPT-IN REQUEST.
- *      THEREFORE, MAKE SURE THAT WHEN USING THIS PARAMETER,YOUR TESTING ENVIRONMENT'S
- *      CLOCK IS SYNCED.
- */
+\define('WP_FS__VVV_DEFAULT_PUBLIC_IP', '192.168.50.4');
 \define('WP_FS__SKIP_EMAIL_ACTIVATION', \false);
 /**
  * Directories
@@ -51,18 +39,20 @@
 \define('WP_FS__DOMAIN_LOCALHOST', 'wp.freemius');
 \define('WP_FS__ADDRESS_LOCALHOST', 'http://' . \WP_FS__DOMAIN_LOCALHOST . ':8080');
 \define('WP_FS__ADDRESS_PRODUCTION', 'https://' . \WP_FS__DOMAIN_PRODUCTION);
+\define('WP_FS__IS_HTTP_REQUEST', isset($_SERVER['HTTP_HOST']));
+\define('WP_FS__REMOTE_ADDR', \fs_get_ip());
 \define('WP_FS__IS_PRODUCTION_MODE', !\defined('WP_FS__DEV_MODE') || !\WP_FS__DEV_MODE || \WP_FS__TESTING_DOMAIN !== $_SERVER['HTTP_HOST']);
 \define('WP_FS__ADDRESS', \WP_FS__IS_PRODUCTION_MODE ? \WP_FS__ADDRESS_PRODUCTION : \WP_FS__ADDRESS_LOCALHOST);
-\define('WP_FS__IS_LOCALHOST', \WP_FS__LOCALHOST_IP == $_SERVER['REMOTE_ADDR']);
-\define('WP_FS__IS_LOCALHOST_FOR_SERVER', \false !== \strpos($_SERVER['HTTP_HOST'], 'localhost'));
+\define('WP_FS__IS_LOCALHOST', \WP_FS__LOCALHOST_IP === \WP_FS__REMOTE_ADDR);
+\define('WP_FS__IS_LOCALHOST_FOR_SERVER', !\WP_FS__IS_HTTP_REQUEST || \false !== \strpos($_SERVER['HTTP_HOST'], 'localhost'));
 \define('FS_API__ADDRESS', 'http://api.freemius:8080');
 \define('FS_API__SANDBOX_ADDRESS', 'http://sandbox-api.freemius:8080');
 \define('WP_FS___OPTION_PREFIX', 'fs' . (\WP_FS__IS_PRODUCTION_MODE ? '' : '_dbg') . '_');
 \define('WP_FS__ACCOUNTS_OPTION_NAME', \WP_FS___OPTION_PREFIX . 'accounts');
 \define('WP_FS__API_CACHE_OPTION_NAME', \WP_FS___OPTION_PREFIX . 'api_cache');
 \define('WP_FS__OPTIONS_OPTION_NAME', \WP_FS___OPTION_PREFIX . 'options');
-\define('WP_FS__IS_HTTPS', isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && 'https' === \strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) && 'on' == $_SERVER['HTTPS'] || isset($_SERVER['SERVER_PORT']) && 443 == $_SERVER['SERVER_PORT']);
-\define('WP_FS__IS_POST_REQUEST', \strtoupper($_SERVER['REQUEST_METHOD']) == 'POST');
+\define('WP_FS__IS_HTTPS', \WP_FS__IS_HTTP_REQUEST && isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && 'https' === \strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) && 'on' == $_SERVER['HTTPS'] || isset($_SERVER['SERVER_PORT']) && 443 == $_SERVER['SERVER_PORT']);
+\define('WP_FS__IS_POST_REQUEST', \WP_FS__IS_HTTP_REQUEST && \strtoupper($_SERVER['REQUEST_METHOD']) == 'POST');
 /**
  * Billing Frequencies
  */
@@ -78,15 +68,15 @@
 /**
  * Times in seconds
  */
-//	define( 'WP_FS__TIME_5_MIN_IN_SEC', 300 );
+\define('WP_FS__TIME_5_MIN_IN_SEC', 300);
 \define('WP_FS__TIME_10_MIN_IN_SEC', 600);
 //	define( 'WP_FS__TIME_15_MIN_IN_SEC', 900 );
 \define('WP_FS__TIME_24_HOURS_IN_SEC', 86400);
 /**
  * Debugging
  */
-\define('WP_FS__DEBUG_SDK', !empty($_GET['fs_dbg']));
-\define('WP_FS__ECHO_DEBUG_SDK', !empty($_GET['fs_dbg_echo']));
+\define('WP_FS__DEBUG_SDK', \WP_FS__DEV_MODE && !empty($_GET['fs_dbg']));
+\define('WP_FS__ECHO_DEBUG_SDK', \WP_FS__DEV_MODE && !empty($_GET['fs_dbg_echo']));
 \define('WP_FS__LOG_DATETIME_FORMAT', 'Y-n-d H:i:s');
 \define('WP_FS__SCRIPT_START_TIME', \time());
 \define('WP_FS__DEFAULT_PRIORITY', 10);
