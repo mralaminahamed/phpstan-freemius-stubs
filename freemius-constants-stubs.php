6535a45da685649579699276c2145d2a75ee27f7
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-12-23 21:42:05 +0600

    Generate stubs for Freemius WordPress SDK 1.1.2

--- freemius-constants-stubs.php
+++ freemius-constants-stubs.php
@@ -7,7 +7,26 @@
 
 \define('WP_FS__SLUG', 'freemius');
 \define('WP_FS__DEV_MODE', \false);
+/**
+ * API Connectivity Simulation
+ */
 \define('WP_FS__SIMULATE_NO_API_CONNECTIVITY', \false);
+\define('WP_FS__SIMULATE_NO_API_CONNECTIVITY_SQUID_ACL', \false);
+\define('FS_SDK__SIMULATE_NO_API_CONNECTIVITY', \true);
+\define('FS_SDK__SIMULATE_NO_API_CONNECTIVITY_SQUID_ACL', \true);
+/**
+ * If true and running with secret key, the opt-in process
+ * will skip the email activation process which is invoked
+ * when the email of the context user already exist in Freemius
+ * database (as a security precaution, to prevent sharing user
+ * secret with unauthorized entity).
+ *
+ * IMPORTANT:
+ *      AS A SECURITY PRECAUTION, WE VALIDATE THE TIMESTAMP OF THE OPT-IN REQUEST.
+ *      THEREFORE, MAKE SURE THAT WHEN USING THIS PARAMETER,YOUR TESTING ENVIRONMENT'S
+ *      CLOCK IS SYNCED.
+ */
+\define('WP_FS__SKIP_EMAIL_ACTIVATION', \false);
 /**
  * Directories
  */
